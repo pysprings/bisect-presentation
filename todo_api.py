88043813221066e3c84380c7080c13295336b218
Ryan Freckleton <ryan.freckleton@gmail.com>
2024-02-26 19:42:58 -0700

    feat(auth): add basic authentication with user credentials

--- todo_api.py
+++ todo_api.py
@@ -4,6 +4,16 @@ from flask_httpauth import HTTPBasicAuth
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
+users = {
+    "john": "hello",
+    "susan": "bye"
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and users[username] == password:
+        return username
+
 # In-memory database for the purpose of this example
 tasks = []
 
